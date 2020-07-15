cab7a9c08997437b88e7d938b680dd037583a619
chrisxue <xavihart@sjtu.edu.cn>
2020-07-15 11:28:32 +0800

    antialising

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from tools.utils import *
 from tools.vec3 import *
 from tools.ray import *
 from tools.hit_list import *
+from tools.camera import *
 import numpy as np
 import sys
 import os
 import timer
 
 
-h = 300
+h = 100
 
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
@@ -54,11 +55,9 @@ def color(r, obj):
 
 def main():
     lower_left_corner = vec3(-2, -1, -1)
-    horizontal = vec3(4, 0, 0)
-    vertical = vec3(0, 2, 0)
-    origin = vec3(0, 0, 0)
-
-    nx, ny = a.shape[0], a.shape[1] // 3
+    cam = camera()
+    ny, nx = a.shape[0], a.shape[1] // 3
+    ns = 100
     print("shape:", nx, ny)
     l = [sphere(vec3(0, -100.5, -1), 100), sphere(vec3(0, 0, -1), 0.5)]
     sp_l = sphere_list(l)
@@ -66,14 +65,26 @@ def main():
         for j in range(ny-1, -1, -1):
             u = i / nx
             v = j / ny
-            r = ray(origin, lower_left_corner + horizontal.mul(v) + vertical.mul(u))
+            col = vec3(0, 0, 0)
+            for s in range(ns):
+                u_, v_ = -1, -1
+                while u_ > 1 or u_ < 0:
+                    u_ = u + np.random.uniform(0, 1e-2)
+                while v_ > 1 or v_ < 0:
+                    v_ = v + np.random.uniform(0, 1e-2)
+                #print(u_, v_)
+                r_ = cam.get_ray(u_, v_)
+                col = col + color(r_, sp_l)
+                
+            col = col.div(ns)
+            # r = ray(origin, lower_left_corner + horizontal.mul(u) + vertical.mul(v))
             # p = r.point_at_parameter(2.0)
-
-            col = color(r, sp_l)
+            # col = color(r, sp_l)
             ir, ig, ib = int(255.99 * col.x()), int(255.99 * col.y()), int(255.99 * col.z())
-            a[i][j*3], a[i][j*3+1], a[i][j*3+2] = ir, ig, ib
+            a[ny - 1 - j][i * 3], a[ny - 1 - j][i * 3 + 1], a[ny - 1 - j][i * 3 + 2] = ir, ig, ib
 
     save_ppm(file_pth, a)
+    
 
 
 main()
